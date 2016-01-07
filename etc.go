c9df8c9b521cb0345b7ff25bcf2e18ccd7320f77
Charalampos Emmanouilidis <chrys.emmanouilidis@gmail.com>
2016-01-07 18:50:27 +0100

    etc.go: Add comments to exported functions and types

--- etc.go
+++ etc.go
@@ -4,10 +4,13 @@ import (
 	"strings"
 )
 
+// OsReleaseFile represents some data of a /etc/os-release file
 type OsReleaseFile struct {
 	ID string
 }
 
+// ReadOsReleaseFile reads a /etc/os-release file and
+// returns the data as an OsReleaseFile struct
 func ReadOsReleaseFile(path string) (*OsReleaseFile, error) {
 	rf := new(OsReleaseFile)
 
@@ -21,11 +24,14 @@ func ReadOsReleaseFile(path string) (*OsReleaseFile, error) {
 	return rf, nil
 }
 
+// LsbReleaseFile represents some data of a /etc/lsb-release file
 type LsbReleaseFile struct {
 	Release  string
 	Codename string
 }
 
+// ReadLsbReleaseFile reads a /etc/lsb-release fie and
+// returns the data as an LsbReleaseFile struct
 func ReadLsbReleaseFile(path string) (*LsbReleaseFile, error) {
 	lsb := new(LsbReleaseFile)
 
